4e663083ad51dce8b254d2537536fcbd991f8f38
davydovil <dawydowi@ya.rul>
2022-04-19 20:55:50 +0300

    test10

--- Работа с API/test.py
+++ Работа с API/test.py
@@ -3,6 +3,6 @@ from smapi import Client
 
 
 
-token = 'bnXpnw48AaqXawOW3OdZso40Q1kVYivv'
+token = 'JzfJSkiCl8kuNuUifZvZqIBEFYVcNp9v'
 client = Client(token)
 print(client.my_homeworks("2022-04-12"))
