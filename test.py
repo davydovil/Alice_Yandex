308d70ef9fc43f8c1dc610c6698373694ad67e0b
davydovil <dawydowi@ya.rul>
2022-04-19 21:46:09 +0300

    Delete files-22

--- test.py
+++ test.py
@@ -1,9 +1,4 @@
 from smapi import Client
-import datetime as dt
-
-
-
-
 token = 'JzfJSkiCl8kuNuUifZvZqIBEFYVcNp9v'
 client = Client(token)
 client.my_homeworks("2022-04-18")
--- Работа с API/__init__.py
+++
@@ -1,1 +0,0 @@
-from .smapi import Client
