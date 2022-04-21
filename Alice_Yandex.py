d684ae8f7babc44c8292a9d8f4401e5b59ddfdae
artem <artem.matrosov.06@mail.ru>
2022-04-21 23:13:15 +0300

    fix start problemo

--- Alice_Yandex.py
+++ Alice_Yandex.py
@@ -4,6 +4,7 @@ from flask import Flask, request
 import logging
 import json
 from smapi import Client
+import datetime
 
 app = Flask(__name__)
 
@@ -13,6 +14,7 @@ sessionStorage = {}
 
 @app.route('/post', methods=['POST'])
 def main():
+    token = ''
     logging.info(f'Request: {request.json!r}')
     response = {
         'session': request.json['session'],
@@ -21,38 +23,35 @@ def main():
             'end_session': False
         }
     }
-    token = ''
-    handle_dialog(request.json, response)
+    handle_dialog(request.json, response, token)
 
     logging.info(f'Response:  {response!r}')
     return json.dumps(response)
 
 
-def handle_dialog(req, res):
+def handle_dialog(req, res, token):
     user_id = req['session']['user_id']
 
-    if req['session']['new']:
-        res['response']['text'] = 'Привет! Я Алиса и я научилась работать с школьным порталом. Для начала работы ' \
-                                  'разрешите мне просматривать ваш ШП ' \
-                                  'Для этого перейдите по ссылке: https://login.school.mosreg.ru/oauth2?response_type=token&client_id=bafe713c96a342b194d040392cadf82b&scope=CommonInfo,ContactInfo,FriendsAndRelatives,EducationalInfo,SocialInfo&redirect_uri=' \
-                                  'После того, как дадите разрешение, скопируйте ссылку страницы, на которую вас перенаправили и отправьте её мне:)'
+    if req['session']['new'] and token == '':
+        res['response']['text'] = 'Разрешите мне просматривать ваш Школьный портал ' \
+                                  'https://login.school.mosreg.ru/oauth2?response_type=token&client_id=bafe713c96a342b194d040392cadf82b&scope=CommonInfo,ContactInfo,FriendsAndRelatives,EducationalInfo,SocialInfo&redirect_uri=' \
+                                  'После скопируйте адрес страницы этой страницы'
 
-        res['response']['tts'] = 'Привет! Я Алиса и я науч+илась работать с школьным порталом. Для начала раб+оты ' \
-                                 'разрешите мне просматривать ваш ШП. Для этого перейдите по ссылке.'
+        res['response']['tts'] = 'Разрешите мне просматривать ваш Школьный портал. Перейдите по ссылке' \
+                                 'и После скопируйте адрес этой страницы'
 
-        return
-
-    if 'https://login.school.mosreg.ru/oauth2/Authorization/Result?response_type=token&client_id' in req['request'][
-        'original_utterance']:
-        # Пользователь согласился, прощаемся.
-        token = req['request']['original_utterance'][255:-7]
-        res['response']['text'] = 'Спасибо, доступ предоставлен, теперь я могу сообщать Вам оценки' \
-                                  f'Ваш токен: {token}'
+        if 'https://login.school.mosreg.ru/oauth2/Authorization/Result?response_type=token&client_id' in req['request'][
+            'original_utterance']:
+            token = req['request']['original_utterance'][255:-7]
+            res['response']['text'] = 'Доступ предоставлен.'
+            return
+    elif req['session']['new'] and token != '':
+        res['response']['text'] = 'Что вам подсказать?'
         return
 
     if 'домашнее задание на завтра' in req['request']['original_utterance']:
-        client = Client('OfN4nuja4zabUX3S0v5Hv3i2f28TnqQn')
-        res['response']['text'] = str(client.get_my_classmates()[1])
+        client = Client(token)
+        res['response']['text'] = client.my_homeworks(datetime.date.today() + datetime.timedelta(days=1))
         return
 
     # Если нет, то убеждаем его купить слона!
